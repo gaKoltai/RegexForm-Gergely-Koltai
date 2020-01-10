0794d686de3b03933677c4e190637ba80f99b3c5
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-01-10 15:35:05 +0100

    Add project files.

---
+++ RegexForm/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RegexForm.ViewModel;
+
+namespace RegexForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            ArrayList errorList = new ArrayList();
+
+            if (!Validator.IsNameValid(TextName.Text))
+            {
+                errorList.Add("Invalid name format, please only use alphabetical characters.");
+            }
+
+            if (!Validator.IsEmailValid(TextMail.Text))
+            {
+                errorList.Add("Invalid email address, please enter a valid email address.");
+            }
+
+            if (!Validator.IsPhoneNumberValid(TextPhone.Text))
+            {
+                errorList.Add("Invalid phone number, please enter a valid phone number");
+            }
+
+            DisplayMessage(errorList);
+        }
+
+        private void DisplayMessage(ArrayList errors)
+        {
+            if (errors.Count > 0)
+            {
+                string errorMessage = "";
+
+                foreach (var error in errors)
+                {
+                    errorMessage += error + "\n";
+                }
+
+                MessageBox.Show("The following errors have occured: \n" + errorMessage);
+            }
+            else
+            {
+                MessageBox.Show("Sucess!");
+            }
+            
+        }
+    }
+    }
---
+++ RegexForm/ViewModel/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace RegexForm.ViewModel
+{
+    class Validator
+    {
+
+        private const string EmailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+        private const string NamePattern = @"^([A-Za-z]+\s*)+$";
+        private const string PhoneNumberPattern = @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$";
+
+
+        private static Match CheckInputForMatch(string input, string pattern)
+        {
+            return Regex.Match(input, pattern);
+        }
+
+        public static bool IsEmailValid(string email)
+        {
+            Match match = CheckInputForMatch(email, EmailPattern);
+
+            return match.Success;
+        }
+
+        public static bool IsPhoneNumberValid(string phone)
+        {
+            Match match = CheckInputForMatch(phone, PhoneNumberPattern);
+
+            return match.Success;
+        }
+
+        public static bool IsNameValid(string name)
+        {
+            Match match = CheckInputForMatch(name, NamePattern);
+
+            return match.Success;
+        }
+
+        public static string ReformatPhoneNumber(string phoneNumber)
+        {
+
+        }
+    }
+}
